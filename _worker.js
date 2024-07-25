bbc9ae85fc3495d7f02267b28c881e142b01515f
zhuzl2021 <85337463+zhuzl2021@users.noreply.github.com>
2024-07-25 10:00:37 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = '8200cdcc-debd-4bb0-99c0-3a363ec22c10';
 
 // https://www.nslookup.io/domains/cdn.xn--b6gac.eu.org/dns-records/
 // https://www.nslookup.io/domains/cdn-all.xn--b6gac.eu.org/dns-records/
